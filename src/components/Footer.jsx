aeb2c6bf2bc07c198fb19025027da5116a00b5b2
Christopher Wooster <woosterwebdev@gmail.com>
2024-07-23 10:07:03 -0700

    Image clean up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ export default function Footer() {
     return (
         <footer className="footer">
             <div className="footer--contacts">
-                <a href="https://github.com/Woosterwebdev"><img className="footer--img" src={github} /></a>
-                <a href="https://www.linkedin.com/in/chris-wooster/"><img className="footer--img" src={linkedin} /></a>
-                <a href="mailto:woosterwebdev@gmail.com"><img className="footer--img" src={email} /></a>
+                <a href="https://github.com/Woosterwebdev" target="_blank"><img className="footer--img" src={github} /></a>
+                <a href="https://www.linkedin.com/in/chris-wooster/" target="_blank"><img className="footer--img" src={linkedin} /></a>
+                <a href="mailto:woosterwebdev@gmail.com" target="_blank"><img className="footer--img" src="/assets/email.svg" /></a>
             </div>
             <p className="footer--content">&copy; 2024 Christopher Wooster</p>
         </footer>
