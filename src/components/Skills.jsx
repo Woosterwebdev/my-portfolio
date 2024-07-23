c581e8e20d8934be7558f49f096061eaf2af002e
Christopher Wooster <woosterwebdev@gmail.com>
2024-07-23 11:37:56 -0700

    Debugging

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,8 +2,7 @@ import React from "react"
 import data from "./data"
   
 export default function Skills() {
-    const skills = data.map(skill => {
-        <img className="skills--img" src={`./assets/${skill.image}`} alt={skill.name} />})
+    const skills = data.map(skill => <img className="skills--img" src={`./assets/${skill.image}`} alt={skill.name} />)
 
     return (
         <section className="skills max-width">
