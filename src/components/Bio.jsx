6280c9509146e2953fc5bac8debdb3821e126e23
Christopher Wooster <woosterwebdev@gmail.com>
2024-07-23 10:23:36 -0700

    Debugging

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,10 +1,9 @@
 import React from "react"
-import chris from '../assets/chris.png'
 
 export default function Bio() {
     return (
         <section className="bio">    
-            <img className="bio--img" src={chris} />
+            <img className="bio--img" src="/assets/chris.png" />
             <h2 className="title m-10">I am a self taught Web Developer</h2>
             <p className="bio--content m-0">Since late 2021, I have been working on learning Web development. I began using Udemy courses in my spare 
                 time. In late 2023 I realized that I needed to commit more time to learning and started on my path of full time education. 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,42 +1,12 @@
 import React from "react"
 import data from "./data"
-import html from '../assets/html.png'
-import css from '../assets/css.png'
-import bootstrap from '../assets/bootstrap.png'
-import js from '../assets/js.png'
-import react from '../assets/react.png'
-import nodejs from '../assets/nodejs.png'
-import npm from '../assets/npm.png'
-import postgre from '../assets/postgre.png'
-import git from '../assets/git.png'
-import github from '../assets/github.svg'
-import firebase from '../assets/firebase.png'
-
-
-// export default function Skills() {
-//     return (
-//         <section className="skills">
-//             <h2 className="title">Skills</h2>
-//             <div className="skills--list">
-//                 <img className="skills--img" src={html} />
-//                 <img className="skills--img" src={css} />
-//                 <img className="skills--img" src={bootstrap} />
-//                 <img className="skills--img" src={js} />
-//                 <img className="skills--img" src={react} />
-//                 <img className="skills--img" src={nodejs} />
-//                 <img className="skills--img" src={npm} />
-//                 <img className="skills--img" src={postgre} />
-//                 <img className="skills--img" src={git} />
-//                 <img className="skills--img" src={github} />
-//                 <img className="skills--img" src={firebase} />
-//             </div>
-//         </section>
-        
-//     )
-// }
-    
+  
 export default function Skills() {
-    const skills = data.map(skill => <img className="skills--img" src={`/assets/${skill.image}`} />)
+    const skills = data.map(skill => {
+        <img 
+        className="skills--img" 
+        src={`/assets/${skill.image}`}
+        />})
     return (
         <section className="skills max-width">
             <h2 className="title">Skills</h2>
