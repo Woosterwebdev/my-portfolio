201767573dd9057937fbe6895103bcead9f74736
Christopher Wooster <woosterwebdev@gmail.com>
2024-10-04 14:52:30 -0700

    Contacts in bio

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-export default function Bio() {
+export default function Bio() {    
     return (
         <section className="bio">
             <img className="bio--img" src="/assets/chris.jpg" alt="Christopher Wooster." />
@@ -10,6 +10,11 @@ export default function Bio() {
                     time. In late 2023 I realized that I needed to commit more time to learning and started on my path of full time education. 
                     I found Scrimba in January 2024 and completed their Frontend Developer Career Path. It was with Scrimba that
                     I really grew in condience and ability.</p>
+                <div className="bio--contacts">
+                    <a href="https://github.com/Woosterwebdev" target="_blank"><img className="footer--img" src="/assets/github.svg" alt="Christopher Wooster's github." /></a>
+                    <a href="https://www.linkedin.com/in/chris-wooster/" target="_blank"><img className="footer--img" src="/assets/linkedin.svg" alt="Christopher Wooster's Linkedin." /></a>
+                    <a href="mailto:woosterwebdev@gmail.com" target="_blank"><img className="footer--img" src="/assets/email.svg" alt="Christopher Wooster's Email." /></a>
+                </div>
             </div>
         </section>
     )
@@ -22,3 +27,18 @@ Image by <a href="https://www.freepik.com/free-vector/realistic-lightning-effect
 
 
 */
+// document.addEventListener('mousemove', function(e) {
+//     const button = document.getElementById('contact-me')
+//     const offset = 100
+//     const xDistance = e.clientX - (button.offsetLeft + button.offsetWidth / 2)
+//     const yDistance = e.clientY - (button.offsetTop + button.offsetHeight / 2)
+//     if (Math.abs(xDistance) < offset && Math.abs(yDistance) < offset) {
+//         const newX = button.offsetLeft + (xDistance > 0 ? -offset : offset)
+//         const newY = button.offsetTop + (yDistance > 0 ? -offset : offset)
+//         const maxX = window.innerWidth - button.offsetWidth
+//         const maxY = window.innerHeight - button.offsetHeight
+    
+//         button.style.left = Math.max(0, Math.min(newX, maxX)) + 'px'
+//         button.style.top = Math.max(0, Math.min(newY, maxY)) + 'px'
+//     }
+// })
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ export default function Footer() {
     return (
         <footer className="footer">
             <div className="footer--contacts">
-                <a href="https://github.com/Woosterwebdev" target="_blank"><img className="footer--img" src="/assets/github.svg" alt="github logo." /></a>
-                <a href="https://www.linkedin.com/in/chris-wooster/" target="_blank"><img className="footer--img" src="/assets/linkedin.svg" alt="Linkedin logo." /></a>
-                <a href="mailto:woosterwebdev@gmail.com" target="_blank"><img className="footer--img" src="/assets/email.svg" alt="Email logo." /></a>
+                <a href="https://github.com/Woosterwebdev" target="_blank"><img className="footer--img" src="/assets/github.svg" alt="Christopher Wooster's github." /></a>
+                <a href="https://www.linkedin.com/in/chris-wooster/" target="_blank"><img className="footer--img" src="/assets/linkedin.svg" alt="Christopher Wooster's Linkedin." /></a>
+                <a href="mailto:woosterwebdev@gmail.com" target="_blank"><img className="footer--img" src="/assets/email.svg" alt="Christopher Wooster's Email." /></a>
             </div>
             <p className="footer--content">&copy; 2024 Christopher Wooster</p>
         </footer>
