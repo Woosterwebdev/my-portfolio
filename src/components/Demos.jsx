73ddc129937d7b42d5b7a12d10629ada2b6716ec
Christopher Wooster <woosterwebdev@gmail.com>
2024-10-04 12:30:19 -0700

    Styling Changes

--- src/components/Demos.jsx
+++ src/components/Demos.jsx
@@ -5,6 +5,17 @@ export default function Demos() {
         <section className="demos max-width">
             <h2 className="title">Demos</h2>
             <div className="project-cards-container">
+                <a href="https://github.com/Woosterwebdev/3DPrintsList" target="_blank">
+                    <div className="project-card">
+                        <div className="project-details">
+                            <h3 className="project-card--title">3D Model Tracker Chrome Extension</h3>
+                            <p>This is a Chrome extension I created to help me track 3D models over various sites. With this app 
+                                I am able to drastically reduce the number of models I lose track of. This link will take you to 
+                                the extensions repo.</p>
+                        </div>
+                        <img className="screenshot" src="/assets/3dprintslist-screenshot.png" alt="3D Print List Chrome Extension." />
+                    </div>
+                </a>
                 <a href="https://reactcomponentlibrary-351329.netlify.app/" target="_blank">
                     <div className="project-card">
                         <div>
@@ -30,16 +41,6 @@ export default function Demos() {
                             <h3 className="project-card--title">Quizzical Movie Trivia App</h3>
                             <p>Using React, JavaScript, and Open Trivia Database API. This movie trivia app has the
                             ability to entertain without falling down a TikTok/Reels/Shorts wormhole and losing track of time.</p>
-                        </div>
-                        <img className="screenshot" src="/assets/quizzical-screenshot.png" alt="Quizzical Movie Trivia App." />
-                    </div>
-                </a>
-                <a href="#" target="_blank">
-                    <div className="project-card">
-                        <div className="project-details">
-                            <h3 className="project-card--title">3D Model Tracker Chrome Extension</h3>
-                            <p>This is a Chrome extension I created to help me track 3D models over various sites. With this app 
-                                I am able to drastically reduce the number of models I lose track of.</p>
                         </div>
                         <img className="screenshot" src="/assets/quizzical-screenshot.png" alt="Quizzical Movie Trivia App." />
                     </div>
