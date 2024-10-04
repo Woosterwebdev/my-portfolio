f3950ba37ac9cdee5066681ffaf0140db480478b
Christopher Wooster <woosterwebdev@gmail.com>
2024-10-04 15:08:03 -0700

    Styling Changes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,14 @@ import React from "react"
 import data from "./data"
   
 export default function Skills() {
-    const skills = data.map(skill => <img key={skill.id} className="skills--img" src={`/assets/${skill.image}`} alt={skill.name} />)
+    const skills = data.map(skill => {
+        return(
+            <div className="skill">
+                <img key={skill.id} className="skills--img" src={`/assets/${skill.image}`} alt={skill.name} />
+                <p>{skill.name}</p>
+            </div>
+    )
+})
 
     return (
         <section className="skills">
